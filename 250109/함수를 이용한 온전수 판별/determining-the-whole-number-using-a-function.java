384adaa647818d1163dddad43ec47e110ce71af2
안병우 <50216870+ByeongwooAhn@users.noreply.github.com>
2025-01-09 17:31:29 +0900

    Title: 함수를 이용한 온전수 판별, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250109/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean wholeNumber(int i) {
+        if(i % 2 == 0) {
+            return false;
+        }
+
+        if(i % 10 == 5) {
+            return false;
+        }
+
+        if(i % 3 == 0 && i % 9 != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int count = 0;
+
+        for(int i = a; i <= b; i++) {
+            if(wholeNumber(i)) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
