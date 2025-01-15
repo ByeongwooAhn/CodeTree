07f088490b120e3a65d415d3065094b3c046c260
안병우 <50216870+ByeongwooAhn@users.noreply.github.com>
2025-01-15 15:35:22 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 250115/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 250115/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static boolean sequence(int [] A, int [] B) {
-        if(A.length < B.length) {
+    public static boolean sequence(int[] A, int[] B) {
+        if (A.length < B.length) {
             return false;
         }
 
-        for(int i = 0; i < B.length; i++) {
-            if(Arrays.equals(Arrays.copyOfRange(A, i, i + B.length), B)) {
+        for (int i = 0; i <= A.length - B.length; i++) {
+            if (Arrays.equals(Arrays.copyOfRange(A, i, i + B.length), B)) {
                 return true;
             }
         }
@@ -22,18 +22,18 @@ public class Main {
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
 
-        int [] A = new int[n1];
-        int [] B = new int[n2];
+        int[] A = new int[n1];
+        int[] B = new int[n2];
 
-        for(int i = 0; i < n1; i++) {
+        for (int i = 0; i < n1; i++) {
             A[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < n2; i++) {
+        for (int i = 0; i < n2; i++) {
             B[i] = sc.nextInt();
         }
 
-        if(sequence(A, B)) {
+        if (sequence(A, B)) {
             System.out.print("Yes");
         } else {
             System.out.print("No");
